4b2e4db0968ca5f85e0744a747769cd0f9fa73b9
IvicaFadljevic <binar@binar-project.com>
2023-01-27 16:32:50 +0100

    prvi commit

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Auth;
+
+class TodosController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // $todos = Todo::all();
+        $todos = Todo::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+        return view('home', compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory
+     */
+    public function create()
+    {
+        return view('add_todo');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:255',
+            'decription' => 'nullable|string',
+            'completed' => 'nullable',
+        ]);
+
+        $todo = new Todo;
+        $todo->title = $request->input('title');
+        $todo->description = $request->input('description');
+
+        if($request->has('completed')) {
+            $todo->completed = true;
+        }
+        $todo->user_id = Auth::user()->id;
+        $todo->save();
+
+        return back()->with('success', 'item created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\View\Factory
+     */
+    public function edit($id)
+    {
+        $todos = Todo::where('user_id', Auth::user()->id)->first();
+        return view('edit_todo', compact('todo'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/edit_todo.blade.php
@@ -0,0 +1,75 @@
+@extends ('layouts.app');
+
+@section('content')
+ <div class="container">
+    <div class="row justify-content-center">
+       <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                      Edit {{ $todo->title}}
+                    </div>
+                    <h5 class="card-header">
+                        <a href="{{ route('todo.index') }}" class="btn btn-sm btn-outline-primary"></a>
+                    </h5>
+                    <div class="card-body">
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach($errors->all() as error)
+                                            <li> {{ $error }} </li>
+                                        @foreach
+                                    </ul>
+                                </div>
+                            @endif
+
+                            @if(session()->has('success'))
+                                <div class="alert alert-success">
+                                    <button type="button" class="close" data-dismiss="alert"> x </button>
+                                    {{ session->get('success')}}
+                                </div>
+                            @endif
+
+                        <form action="POST" action="{{ route('todo.update') }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group row">
+                                <label for="title" type="title" class="col-form-label text-md-right"> Title </label>
+                                <input id="title" type="title" class="form-control @error('title') is-invalid @enderror" name="title" value = "{{ $todo->title}}" required autocomplete="title" autofocus>
+                                @error for="title"
+                                    <span class="invalid-callback" role="alert">
+                                        <strong> {{ $message }} </strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="description" class="col-form-label text-md-right"> Decription </label>
+                                <textarea name="decription" id="description" cols="30" rows="10" class="form-control @error ('password') is-invalid @enderror" autocomplete="description" value="{{ $todo->description }}"></textarea>
+                                @error
+                                    <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" name="completed" value=" {{ $todo->completed}}">
+                                    <label class="form-check-label" for="completed"> Completed? </label>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="button" class="btn btn-primary">
+                                        Sumbit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                   </div>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
